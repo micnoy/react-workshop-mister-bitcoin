9d655796f9d27045cb3fc59b8ef58e97c157e614
Michael Noy <michael@zemingo.com>
2019-12-19 15:21:08 +0200

    Updates

---
+++ precache-manifest.ae91d0dac47ff76307b88d120f496aa5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ccb1658bbcce0e08fc3a2ddba35a65",
+    "url": "/react-workshop-mister-bitcoin/index.html"
+  },
+  {
+    "revision": "de71c6ee66681aacb53b",
+    "url": "/react-workshop-mister-bitcoin/static/css/main.7df392c2.chunk.css"
+  },
+  {
+    "revision": "52aab5e1431e24836a15",
+    "url": "/react-workshop-mister-bitcoin/static/js/2.40531a1e.chunk.js"
+  },
+  {
+    "revision": "eb89643b19c316b7732aae51613d6329",
+    "url": "/react-workshop-mister-bitcoin/static/js/2.40531a1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de71c6ee66681aacb53b",
+    "url": "/react-workshop-mister-bitcoin/static/js/main.7fc32ef8.chunk.js"
+  },
+  {
+    "revision": "711478b81238108eeb45",
+    "url": "/react-workshop-mister-bitcoin/static/js/runtime-main.d675515a.js"
+  },
+  {
+    "revision": "c56851b8ab3bc7182fb232e8752e6f37",
+    "url": "/react-workshop-mister-bitcoin/static/media/ic-contacts.c56851b8.svg"
+  },
+  {
+    "revision": "5924881cdb4163556797480ce5af5774",
+    "url": "/react-workshop-mister-bitcoin/static/media/ic-home.5924881c.svg"
+  },
+  {
+    "revision": "2e8a0200458fc9bf8cbd320f20e5c274",
+    "url": "/react-workshop-mister-bitcoin/static/media/ic-statistics.2e8a0200.svg"
+  }
+]);
