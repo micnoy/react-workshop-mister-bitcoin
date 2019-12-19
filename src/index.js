6b57b541aff434499ff38f06c56d6b953be4814f
Michael Noy <michael@zemingo.com>
2019-12-19 16:04:04 +0200

    testing adding install app

--- src/index.js
+++ src/index.js
@@ -10,15 +10,50 @@ import store from "./store"
 
 const history = createBrowserHistory();
 
+let deferredPrompt; // Allows to show the install prompt
+let setupButton;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    console.log("beforeinstallprompt fired");
+    if (setupButton == undefined) {
+        setupButton = document.getElementById("setup_button");
+    }
+    // Show the setup button
+    setupButton.style.display = "inline";
+    setupButton.disabled = false;
+});
+
+function installApp() {
+    // Show the prompt
+    deferredPrompt.prompt();
+    setupButton.disabled = true;
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+        .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('PWA setup accepted');
+                // hide our user interface that shows our A2HS button
+                setupButton.style.display = 'none';
+            } else {
+                console.log('PWA setup rejected');
+            }
+            deferredPrompt = null;
+        });
+}
+
 ReactDOM.render(
+
     <Provider store={store}>
         <Router history={history}>
             <App/>
         </Router>
     </Provider>
     , document.getElementById('root'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
